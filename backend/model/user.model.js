01a08cd45869ddd66f6ac02de1bdcdc8736ababf
ImKetan1610 <ketan.makode@gmail.com>
2023-04-09 16:42:37 +0530

    updated index.js

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    match: /^[^\s@]+@[\s@]+\.[^\s@]+$/,
+    // match: /^[^\s@]+@[\s@]+\.[^\s@]+$/,
   },
   bio: { type: String, maxlength: 200 },
   created_at: { type: Date, default: Date.now },
